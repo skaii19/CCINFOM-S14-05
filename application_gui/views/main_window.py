ece84ade397939c770ab5e31ff51a8694fe8befb
skaii19 <julienneskye@gmail.com>
2025-11-19 20:41:28 +0800

    Update main_window.py

--- application_gui/views/main_window.py
+++ application_gui/views/main_window.py
@@ -3,7 +3,6 @@ from views.player_tab import PlayerTab
 from views.teams_tab import TeamsTab
 from views.tournaments_tab import TournamentsTab
 from views.matches_tab import MatchesTab
-from views.customers_tab import CustomersTab
 from views.merchandise_tab import MerchandiseTab
 from views.reports_tab import ReportTab
 
@@ -23,7 +22,6 @@ class MainWindow(QMainWindow):
         self.teams_tab = TeamsTab()
         self.tournaments_tab = TournamentsTab()
         self.matches_tab = MatchesTab()
-        self.customers_tab = CustomersTab()
         self.merch_tab = MerchandiseTab()
         self.reports_tab = ReportTab()
 
@@ -32,6 +30,5 @@ class MainWindow(QMainWindow):
         self.tabs.addTab(self.teams_tab, "Teams")
         self.tabs.addTab(self.tournaments_tab, "Tournaments")
         self.tabs.addTab(self.matches_tab, "Matches")
-        self.tabs.addTab(self.customers_tab, "Customers")
         self.tabs.addTab(self.merch_tab, "Merchandise")
         self.tabs.addTab(self.reports_tab, "Reports")
